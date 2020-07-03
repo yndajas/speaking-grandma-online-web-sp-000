fbd785288ede3185ba7539534e96755b2dc1ee34
Ynda Jas <yndajas@gmail.com>
2020-07-03 02:15:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase
     puts "NO, NOT SINCE 1938!"
   else
